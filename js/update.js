293b53541c8d57ec099b5370367daf0d46a79605
Vincent <vinz2007@hotmail.com>
2014-01-25 12:45:00 +0100

    France population density

--- js/update.js
+++ js/update.js
@@ -18,33 +18,34 @@ function dptPop(year){
 		return(d.year==yearPicked)});
 
 	console.log(popData);
-	var minpop = d3.min(data.map(function(d) {return (d.value);} ))-1;
-	var maxpop = d3.max(data.map(function(d) {return (d.value);} ))+1;
+	var minPop = d3.min(data.map(function(d) {return (d.density);} ));
+	var maxPop = d3.max(data.map(function(d) {return (d.density);} ))+1;
 
-	console.log(minpop);
-	console.log(maxpop);
+	console.log(minPop);
+	console.log(maxPop);
 
 	var color = d3.scale.threshold()
-	    .domain([minpop,minpop+(maxpop-minpop)/9,minpop+2*(maxpop-minpop)/9,minpop+3*(maxpop-minpop)/9,minpop+4*(maxpop-minpop)/9,minpop+5*(maxpop-minpop)/9,minpop+6*(maxpop-minpop)/9,minpop+7*(maxpop-minpop)/9,minpop+8*(maxpop-minpop)/9,maxpop])
+	    .domain([minPop,minPop+1*(maxPop-minPop)/(1000),minPop+2*(maxPop-minPop)/(1000),minPop+3*(maxPop-minPop)/(1000),minPop+4*(maxPop-minPop)/(1000),minPop+5*(maxPop-minPop)/(1000),minPop+6*(maxPop-minPop)/(1000),minPop+7*(maxPop-minPop)/(1000),minPop+8*(maxPop-minPop)/(1000),maxPop])
+	    //.domain([minpop,minpop+(maxpop-minpop)/999,minpop+2*(maxpop-minpop)/999,minpop+3*(maxpop-minpop)/999,minpop+4*(maxpop-minpop)/999,minpop+5*(maxpop-minpop)/999,minpop+6*(maxpop-minpop)/999,minpop+7*(maxpop-minpop)/999,minpop+8*(maxpop-minpop)/999,maxpop])
 	    .range(["#fff","#fff7ec", "#fee8c8", "#fdd49e", "#fdbb84", "#fc8d59", "#ef6548","#d7301f","#b30000","#7f0000"]);
 
 
 
 	d3.json("data/dpt2.json", function(error, dptFrance) {	
   		var rateById = {};
-		popData.forEach(function(d) { rateById[d.dpt] = d.value; });
+		popData.forEach(function(d) { rateById[d.dpt] = d.density; });
 		
 		console.log(rateById);
 		console.log(color(rateById));
 		dpts.selectAll("path")
 			.data(dptFrance.features)
-		    .transition().duration(1500)
+		    .transition().duration(500)
 	        .style("fill", function(d) { return color(rateById[d.id]); });
 
 	var  populationTotal =0;
 	popData.forEach(function(d) { populationTotal = populationTotal + d.value; });
 	
-	var populationTotalFrance = "France total population: " + format(populationTotal) + " inhabitants"
+	var populationTotalFrance = "France density: " + d3.round(populationTotal/552000,1) + " inhabitants/km²"
 	
 	g.append("text")
 	    .attr("class", "caption")
@@ -87,13 +88,26 @@ function dptPop(year){
 		        		 dptValue=n.value;
 		        });
 
+			console.log(dptValue);
+
+			var dptDensity;
+			popData.forEach(function(n){
+		        	if (n.dpt==d.id)
+		        		 dptDensity=n.density;
+		        });
+			console.log(dptDensity);
+
+			d3.select("#tooltip")
+		        .select("#densityTooltip")
+		        .text(format(d3.round(dptDensity,1)));
+
 			d3.select("#tooltip")
 		        .select("#valueTooltip")
 		        .text(format(dptValue));
 			
 
-			var dptGrowth;
-			if (yearPicked!=1962){
+			/*var dptGrowth;
+			if (yearPicked!=1931){
 				popData.forEach(function(n){
 			        	if (n.dpt==d.id)
 			        		if(n.growth>=0){
@@ -107,7 +121,7 @@ function dptPop(year){
 
 			d3.select("#tooltip")
 		        .select("#growthTooltip")
-		        .text(dptGrowth);
+		        .text(dptGrowth);*/
 
 
 			d3.select("#tooltip").classed("hidden", false);
